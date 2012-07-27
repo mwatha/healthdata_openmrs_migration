cfd4f8b9257d3a60a269caffc809af3f88c274f4
mwatha <mwatha.bwanalie@baobabhealth.org>
2012-07-27 16:03:26 +0200

    working on migrating Radiology data - 60% done

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,5 +2,7 @@ class Patient < ActiveRecord::Base
     establish_connection "openmrs_#{Rails.env}"
     self.table_name = 'patient'
 
+    has_many :patient_identifiers, :foreign_key => :patient_id, 
+      :dependent => :destroy, :conditions => {:voided => 0}
 
 end
